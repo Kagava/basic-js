ba769dbabb0bc29dbb80313062bde5deed714bd7
Kagava <esspakov@mail.ru>
2025-11-10 20:31:14 +0700

    feat: done simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,30 +1,66 @@
-const { decorateObject } = require('../lib');
-const { NotImplementedError } = require('../lib');
+const { decorateObject } = require("../lib");
+const { NotImplementedError } = require("../lib");
 
 /**
  * Implement chainMaker object according to task description
  *
  */
 const chainMaker = {
+  chain: "",
+  len: 0,
   getLength() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.len;
   },
-  addLink(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    if (this.len === 0) {
+      this.chain += `( ${value} )`;
+    } else {
+      this.chain += `~~( ${value} )`;
+    }
+    this.len += 1;
+    return this;
   },
-  removeLink(/* position */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    console.log(position);
+    if (isNaN(+position)) throw new Error("You can't remove incorrect link!");
+
+    if (position - 1 < 0 || position > this.len)
+      throw new Error("You can't remove incorrect link!");
+    position -= 1;
+    const regex = /\(\s.+?\s\)/g;
+    const chainArray = this.chain.match(regex);
+    const beginChainIndex =
+      position === 0
+        ? this.chain.indexOf(chainArray[position])
+        : this.chain.indexOf(chainArray[position]) - 2;
+    const beginChain = this.chain.slice(0, beginChainIndex);
+    const endChain = this.chain.slice(
+      beginChainIndex + chainArray[position].length + 2,
+      this.chain.length
+    );
+    this.chain = `${beginChain}${endChain}`;
+    this.len -= 1;
+    return this;
   },
   reverseChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (this.len < 2) return this;
+    const regex = /\(\s.+?\s\)/g;
+    const chainArray = this.chain.match(regex);
+    this.chain = "";
+    for (let i = chainArray.length - 1; i >= 0; i -= 1) {
+      if (i === chainArray.length - 1) {
+        this.chain += `${chainArray[i]}`;
+      } else {
+        this.chain += `~~${chainArray[i]}`;
+      }
+    }
+    return this;
   },
   finishChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    const newChain = this.chain;
+    this.chain = "";
+    this.len = 0;
+    return newChain;
   },
 };
 
