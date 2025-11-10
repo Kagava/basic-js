805e11f8f5ed6f3094a3a1ff315a3c52846538a6
Kagava <esspakov@mail.ru>
2025-11-10 14:42:40 +0700

    feat: done extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Create a repeating string based on the given parameters
@@ -16,11 +16,36 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function repeater(/* str, options */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  let ansStr = "";
+  const repatTimes = options.repeatTimes ? options.repeatTimes : 1;
+  const additionRepeatTimes = options.additionRepeatTimes
+    ? options.additionRepeatTimes
+    : 1;
+  console.log(repatTimes, additionRepeatTimes);
+  for (let i = 0; i < repatTimes; i += 1) {
+    ansStr += str;
+    for (let j = 0; j < additionRepeatTimes; j += 1) {
+      ansStr += options.addition !== undefined ? options.addition : "";
+      if (j !== additionRepeatTimes - 1) {
+        if (options.additionSeparator) {
+          ansStr += options.additionSeparator;
+        } else {
+          ansStr += "|";
+        }
+      }
+    }
+    if (i !== repatTimes - 1) {
+      if (options.separator) {
+        ansStr += options.separator;
+      } else {
+        ansStr += "+";
+      }
+    }
+  }
+  return ansStr;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
